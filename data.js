f994d572c892224a492cbdd2d8cc7a09fac5e9b6
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-01-19 20:56:43 -0800

    added some css

--- data.js
+++ data.js
@@ -6,6 +6,29 @@ export const vintageItem = [
         price: 65,
         color: 'red',
         size: 10,
-        
     },
+    {
+        image: 1930,
+        id: 2,
+        name: 'Shoe Horn',
+        price: 100,
+        color: 'brass',
+        size: 'small',
+    },
+    {
+        image: 1950,
+        id: 3,
+        name: 'Belt Buckle',
+        price: 165,
+        color: 'silver',
+        size: 'large',
+    },
+    {
+        image: 1910,
+        id: 4,
+        name: 'Spirs',
+        price: 25,
+        color: 'gold',
+        size: 'large',
+    }
 ];
